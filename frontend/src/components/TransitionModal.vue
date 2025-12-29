2bf4923141a3e9aaf8d0d5bd27cea4f0e2f4a489
Mark Ramos <mjramos76@gmail.com>
2025-12-28 16:44:23 -0800

    feat(components): Add TransitionModal wrapper component

---
+++ frontend/src/components/TransitionModal.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  visible: boolean;
+}>();
+
+defineEmits<{
+  'backdrop-click': [];
+}>();
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition
+      enter-from-class="modal-backdrop-enter-from"
+      enter-active-class="modal-backdrop-enter-active"
+      leave-to-class="modal-backdrop-leave-to"
+      leave-active-class="modal-backdrop-leave-active"
+    >
+      <div
+        v-if="visible"
+        class="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+        @click.self="$emit('backdrop-click')"
+      >
+        <Transition
+          enter-from-class="modal-enter-from"
+          enter-active-class="modal-enter-active"
+          leave-to-class="modal-leave-to"
+          leave-active-class="modal-leave-active"
+          appear
+        >
+          <slot />
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
